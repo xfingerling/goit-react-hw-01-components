8c8bbd48d4775f1577a6f1c96b22eb36484d8d3f
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-04-02 22:35:14 +0300

    Updates

---
+++ precache-manifest.0ac828ea0b2ac9b05840df3dce4c7e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efde8a16eb287ee04377179defbf1ed",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "aea9cdaebe10bbd17ec2",
+    "url": "/goit-react-hw-01-components/static/css/main.ab9c32d9.chunk.css"
+  },
+  {
+    "revision": "71126d47341ff173c5c6",
+    "url": "/goit-react-hw-01-components/static/js/2.0beb12a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.0beb12a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea9cdaebe10bbd17ec2",
+    "url": "/goit-react-hw-01-components/static/js/main.551e2a63.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
